840aa45e3e8e52398d679b5b9d55987cd480dece
Mayur Mohurle <mayur.mohurle880606@gmail.com>
2021-08-21 13:13:18 +0530

    1st commit

---
+++ src/counterSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1
+    },
+    decrement: (state) => {
+      state.value -= 1
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
+
+console.log(increment,"INCREMENT")
+console.log(counterSlice.reducer,"COUNTERSLICE REDUCEER")
+console.log(counterSlice.actions,"COUNTERSLICE Action")
+
+export default counterSlice.reducer
